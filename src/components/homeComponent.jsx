295f42b6c0acf657a24b67f087c427243b926a0c
Olabamipe Taiwo <teeola48@gmail.com>
2019-08-14 13:19:00 +0000

    Absolute Shoes Placed

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -11,7 +11,10 @@ const Home = () => {
                     <h1 className="home-h1">Tide< span className="home-span">1.0</span></h1>
                     <h5 className="home-h5">FEEL EMPOWERED IN TIDE</h5>
                     <button className="home-btn">SHOP NOW</button>
+                    <img className="home-pinkshoe" src="./img/pink.png" alt="pinkshoe" />
+                    <img className="home-whiteshoe" src="./img/white.png" alt="whiteshoe" />
                  </div>
+               
              </Fragment>
           
        );
